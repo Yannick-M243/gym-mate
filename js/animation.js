89972bdec4c436616a9cdba53adcf246896c5def
Yannick Makwenge <yannickmakwenge@gmail.com>
2022-05-31 23:14:23 +0200

    creating plans page

---
+++ js/animation.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  /* $(".item-container").mouseover(function () {
+    // $(".item-desc").css("display", "block");
+    $(".item-desc").stop().slideUp();
+  });
+  $(".item-container").mouseout(function () {
+    $(".item-desc").slideDown();
+  });*/
+});
